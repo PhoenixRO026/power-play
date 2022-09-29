30840e3cb9f3219bec5ae69bdda04946a4bc7ef9
ProNoob1029 <pronoob1029@gmail.com>
2022-09-29 16:07:16 +0300

    test pt motor si servo

---
+++ TeamCode/src/main/java/CursProgramare/TestMecanica.java
@@ -0,0 +1,22 @@
+package CursProgramare;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "test mecanica")
+public class TestMecanica extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "motor");
+        Servo servo = hardwareMap.get(Servo.class, "servo");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            motor.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+            servo.setPosition(gamepad1.left_stick_y);
+        }
+    }
+}
