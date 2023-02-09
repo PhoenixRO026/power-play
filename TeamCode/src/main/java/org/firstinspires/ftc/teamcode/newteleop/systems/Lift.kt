90803476c1c8fcc0d93999d0f5947574d34802af
ProNoob1029 <pronoob1029@gmail.com>
2023-02-09 21:11:40 +0200

    fixed imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newteleop/systems/Lift.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newteleop/systems/Lift.kt
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
-import kotlin.math.abs
 import kotlin.math.sign
 
 class Lift(
