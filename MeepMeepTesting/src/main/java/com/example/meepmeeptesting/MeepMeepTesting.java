47da35cf4b50866aeeb7fb220b5e4799b01c09cb
Dragos Morar <pronoob1029@gmail.com>
2023-02-16 10:53:12 +0200

    testing riskier auto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -6,7 +6,6 @@ import static com.example.constants.Constants.MAX_ANG_VEL;
 import static com.example.constants.Constants.MAX_VEL;
 import static com.example.constants.Constants.TRACK_WIDTH;
 
-import com.example.constants.Constants;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -14,7 +13,215 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        meep();
+        meep2();
+    }
+
+    public static void meep2() {
+        MeepMeep meepMeep = new MeepMeep(650);
+
+        double fieldSize = 141.1;
+        double driveTrainLenght = 14.1;
+
+        ConfigPose startPose = new ConfigPose(
+                fieldSize / 4,
+                -fieldSize / 2 + driveTrainLenght / 2,
+                Math.toRadians(90)
+        );
+
+        ConfigPose pose1 = new ConfigPose(
+                fieldSize / 4,
+                -fieldSize / 12,
+                Math.toRadians(180)
+        );
+
+        ConfigPose midPose = new ConfigPose(
+                fieldSize / 6,
+                -fieldSize / 6,
+                Math.toRadians(270 - 45)
+        ).offset(0, -driveTrainLenght / 2 - 2);
+
+        ConfigPose highPose = new ConfigPose(
+                fieldSize / 6,
+                0,
+                Math.toRadians(90 + 45)
+        ).offset(0, -driveTrainLenght / 2 - 2);
+
+        ConfigPose pose2 = new ConfigPose(
+                fieldSize / 4,
+                -fieldSize / 12,
+                Math.toRadians(270)
+        );
+
+        ConfigPose stackPose = new ConfigPose(
+                fieldSize / 2.4 - 1,
+                -fieldSize / 12,
+                Math.toRadians(0)
+        );
+
+        ConfigPose pose3 = new ConfigPose(
+                fieldSize / 4,
+                -fieldSize / 12,
+                Math.toRadians(270)
+        );
+
+        ConfigPose pose4 = new ConfigPose(
+                fieldSize / 4,
+                -fieldSize / 12,
+                Math.toRadians(90)
+        );
+
+        RoadRunnerBotEntity myBot3 = new DefaultBotBuilder(meepMeep)
+                .setDimensions(driveTrainLenght, driveTrainLenght)
+                .setConstraints(MAX_VEL, MAX_ACCEL, MAX_ANG_VEL, MAX_ANG_ACCEL, TRACK_WIDTH)
+                .setColorScheme(new ColorSchemeRedDark())
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(startPose.pose2d())
+                                .lineToLinearHeading(pose1.pose2d())
+                                .lineToLinearHeading(midPose.pose2d())
+                                .UNSTABLE_addTemporalMarkerOffset(-2, () -> {})
+                                .waitSeconds(0.5)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose2.pose2d())
+                                .lineToLinearHeading(stackPose.pose2d())
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.3)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose3.pose2d())
+                                .lineToLinearHeading(midPose.pose2d())
+                                .UNSTABLE_addTemporalMarkerOffset(-2, () -> {})
+                                .waitSeconds(0.5)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose2.pose2d())
+                                .lineToLinearHeading(stackPose.pose2d())
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.3)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose3.pose2d())
+                                .lineToLinearHeading(midPose.pose2d())
+                                .UNSTABLE_addTemporalMarkerOffset(-2, () -> {})
+                                .waitSeconds(0.5)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose2.pose2d())
+                                .lineToLinearHeading(stackPose.pose2d())
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.3)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose4.pose2d())
+                                .lineToLinearHeading(highPose.pose2d())
+                                .UNSTABLE_addTemporalMarkerOffset(-2, () -> {})
+                                .waitSeconds(0.5)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose4.pose2d())
+                                .strafeRight(fieldSize / 6)
+                                .build()
+                );
+
+        RoadRunnerBotEntity myBot4 = new DefaultBotBuilder(meepMeep)
+                .setDimensions(driveTrainLenght, driveTrainLenght)
+                .setConstraints(MAX_VEL, MAX_ACCEL, MAX_ANG_VEL, MAX_ANG_ACCEL, TRACK_WIDTH)
+                .setColorScheme(new ColorSchemeRedDark())
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(startPose.reversed().pose2d())
+                                .lineToLinearHeading(pose1.reversed().pose2d())
+                                .lineToLinearHeading(midPose.reversed().pose2d())
+                                .UNSTABLE_addTemporalMarkerOffset(-2, () -> {})
+                                .waitSeconds(0.5)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose2.reversed().pose2d())
+                                .lineToLinearHeading(stackPose.reversed().pose2d())
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.3)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose3.reversed().pose2d())
+                                .lineToLinearHeading(midPose.reversed().pose2d())
+                                .UNSTABLE_addTemporalMarkerOffset(-2, () -> {})
+                                .waitSeconds(0.5)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose2.reversed().pose2d())
+                                .lineToLinearHeading(stackPose.reversed().pose2d())
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.3)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose3.reversed().pose2d())
+                                .lineToLinearHeading(midPose.reversed().pose2d())
+                                .UNSTABLE_addTemporalMarkerOffset(-2, () -> {})
+                                .waitSeconds(0.5)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose2.reversed().pose2d())
+                                .lineToLinearHeading(stackPose.reversed().pose2d())
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.3)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose4.reversed().pose2d())
+                                .lineToLinearHeading(highPose.reversed().pose2d())
+                                .UNSTABLE_addTemporalMarkerOffset(-2, () -> {})
+                                .waitSeconds(0.5)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .waitSeconds(0.2)
+                                .addTemporalMarker(() -> {})
+                                .lineToLinearHeading(pose4.reversed().pose2d())
+                                .strafeLeft(fieldSize / 6)
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_KAI_DARK)
+                .setDarkMode(true)
+                // Background opacity from 0-1
+                .setBackgroundAlpha(0.95f)
+                //.addEntity(myBot1)
+                //.addEntity(myBot2)
+                .addEntity(myBot3)
+                .addEntity(myBot4)
+                //.addEntity(myBot4)
+                .start();
     }
 
     public static void meep() {
